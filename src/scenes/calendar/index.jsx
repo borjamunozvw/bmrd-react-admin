21323f802dfb04b46e0a3015a42f4679eb55392a
borjamunozvw <102975279+borjamunozvw@users.noreply.github.com>
2023-02-02 17:33:26 +0100

    deleting nodemodules

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-// import FullCalendar, { formatDate } from "@fullcalendar/react";
+import FullCalendar from "@fullcalendar/react";
+import formatDate from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-// import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
 import {
   Box,
@@ -14,7 +15,6 @@ import {
 } from "@mui/material";
 import { Header } from "../../components/Header";
 import { tokens } from "../../theme";
-import { formatDate } from "@fullcalendar/core";
 
 const Calendar = () => {
   const theme = useTheme();
@@ -82,6 +82,43 @@ const Calendar = () => {
               </ListItem>
             ))}
           </List>
+        </Box>
+        {/* CALENDAR */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "12315",
+                title: "All-day event",
+                date: "2022-09-14",
+              },
+              {
+                id: "5123",
+                title: "Timed event",
+                date: "2022-09-28",
+              },
+            ]}
+          />
         </Box>
       </Box>
     </Box>
